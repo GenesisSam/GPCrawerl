8c915a7bfb097cb5c13d5c9d11856262e82ccbfa
samuel-dh-lee <samuel940401@gmail.com>
2016-07-06 15:53:38 +0900

    쓰레드 돌연사 해결,  탐색할 카테고리 정의됨

--- main.py
+++ main.py
@@ -15,6 +15,70 @@ thd_flag = False
 thrd = []
 
 f = "https://play.google.com"
+uuu = [
+    "/store/apps",
+    "/store/apps/category/GAME",
+    "/store/apps/category/FAMILY",
+    "/store/apps/category/LIFESTYLE",
+"/store/apps/category/COMICS",
+"/store/apps/category/MEDIA_AND_VIDEO",
+"/store/apps/category/BUSINESS",
+"/store/apps/category/PHOTOGRAPHY",
+"/store/apps/category/PRODUCTIVITY",
+"/store/apps/category/SOCIAL",
+"/store/apps/category/SHOPPING",
+"/store/apps/category/SPORTS",
+"/store/apps/category/ENTERTAINMENT",
+"/store/apps/category/TRAVEL_AND_LOCAL",
+"/store/apps/category/APP_WIDGETS",
+"/store/apps/category/MUSIC_AND_AUDIO",
+"/store/apps/category/MEDICAL",
+"/store/apps/category/COMMUNICATION",
+"/store/apps/category/ANDROID_WEAR",
+"/store/apps/category/GAME",
+"/store/apps/category/GAME_EDUCATIONAL",
+"/store/apps/category/GAME_WORD",
+"/store/apps/category/GAME_ROLE_PLAYING",
+"/store/apps/category/GAME_BOARD",
+"/store/apps/category/GAME_SPORTS",
+"/store/apps/category/GAME_SIMULATION",
+"/store/apps/category/GAME_ARCADE",
+"/store/apps/category/GAME_ACTION",
+"/store/apps/category/GAME_ADVENTURE",
+"/store/apps/category/GAME_MUSIC",
+"/store/apps/category/GAME_RACING",
+"/store/apps/category/GAME_STRATEGY",
+"/store/apps/category/GAME_CARD",
+"/store/apps/category/GAME_CASINO",
+"/store/apps/category/GAME_CASUAL",
+"/store/apps/category/GAME_TRIVIA",
+"/store/apps/category/GAME_PUZZLE",
+"/store/apps/category/FAMILY",
+"/store/apps/category/FAMILY?age=AGE_RANGE1",
+"/store/apps/category/FAMILY?age=AGE_RANGE2",
+"/store/apps/category/FAMILY?age=AGE_RANGE3",
+"/store/apps/category/FAMILY_EDUCATION",
+"/store/apps/category/FAMILY_BRAINGAMES",
+"/store/apps/category/FAMILY_ACTION",
+"/store/apps/category/FAMILY_PRETEND",
+"/store/apps/category/FAMILY_MUSICVIDEO",
+"/store/apps/category/FAMILY_CREATE",
+    "/store/apps/category/HEALTH_AND_FITNESS",
+    "/store/apps/category/PERSONALIZATION",
+    "/store/apps/category/WEATHER",
+    "/store/apps/category/TOOLS",
+    "/store/apps/category/FINANCE",
+    "/store/apps/category/EDUCATION",
+    "/store/apps/category/TRANSPORTATION",
+    "/store/apps/category/NEWS_AND_MAGAZINES",
+    "/store/apps/category/BOOKS_AND_REFERENCE",
+    "/store/apps/category/APP_WALLPAPER",
+    "/store/apps/category/LIBRARIES_AND_DEMO",
+"/store/apps/category/MEDIA_AND_VIDEO/collection/topselling_paid",
+"/store/apps/category/MEDIA_AND_VIDEO/collection/topselling_paid",
+"/store/apps/category/MEDIA_AND_VIDEO/collection/topselling_free",
+"/store/apps/category/MEDIA_AND_VIDEO/collection/topselling_free"]
+
 
 def main():
     global datas
@@ -23,42 +87,45 @@ def main():
 
 
     # url = "https://play.google.com/store/apps"
-    urls.append("https://play.google.com/store/apps")
+
+    for d in uuu:
+        urls.append(f+d)
 
     while len(urls) > 0:
 
-        for url in urls:
+        url = urls.pop(0)
 
-            urls.pop(0)
+        print "\t[-] url left: %d" % len(urls)
 
-            print "\t[-] url left: %d" % len(urls)
+        res = requests.get(url)
 
-            res = requests.get(url)
+        if res.status_code == 200:
+            html = res.text
+            soup = BeautifulSoup(html, "html.parser")
 
-            if res.status_code == 200:
-                html = res.text
-                soup = BeautifulSoup(html, "html.parser")
+            for d in soup.find_all("a"):
+                d = d.get("href")
 
-                for d in soup.find_all("a"):
-                    d = d.get("href")
+                p = re.compile(r"^/store/apps/details\?id=")
+                p2 = re.compile(r"^/store/apps/[developer|dev]+\?id=")
+                p3 = re.compile(r"^/store/apps/category/[a-zA-Z]+")
 
-                    p = re.compile(r"^/store/apps/details\?id=")
-                    p2 = re.compile(r"^/store/apps/[developer|dev]+\?id=")
-                    p3 = re.compile(r"^/store/apps/category/[a-zA-Z]+")
+                if p.match(d):
+                    datas.append(f+d)
+                # elif p2.match(d):
+                #     if len(urls) > 500:
+                #         return
+                #
+                #     urls.append(f+d)
+                #     urls = list(set(urls))
+            print "[+] Added, %d" % len(datas)
 
-                    if p.match(d):
-                        datas.append(f+d)
-                    elif p2.match(d) or p3.match(d):
-                        if len(urls) > 500:
-                            return
 
-                        urls.append(f+d)
-                        urls = list(set(urls))
-                print "[+] Added, %d" % len(datas)
 
 def getData(name):
-    for i in datas:
-        datas.pop(0)
+
+    while len(datas) > 0:
+        i = datas.pop(0)
         print "\t[- %s] data left: %d" % (name, len(datas))
         if thd_flag == True:
             break
@@ -129,16 +196,21 @@ def getData(name):
                 f.write("\n")
 
 def thr():
-    for i in range(0, 5):
 
-        if len(thrd) < 5:
-            if len(thrd) >= 0 and len(thrd) <= 5:
-                print "==============================="
-                print "[+] Thread created and run"
-                print "==============================="
-                thrd.append(threading.Thread(target=getData, args=(str(i))).start())
-            else:
-                pass
+    for i in range(0, 5):
+        print "==============================="
+        print "[+] Thread created and run"
+        print "==============================="
+        thrd.append(threading.Thread(target=getData, args=(str(i))).start())
+
+    for i in range(0, len(thrd)):
+        try:
+            thrd[i].join()
+        except AttributeError as err:
+            pass
+
+    print "[+] All process work done"
+
 
 if __name__=="__main__":
     try:
